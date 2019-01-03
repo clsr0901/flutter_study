456f52e9b585ed8722cae39889b7072c4a1bd66c
chenls520 <406797103@qq.com>
2019-01-03 20:45:04 +0800

    flutter初始化

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new RandomWords()
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new RandomWordsState();
+  }
+}
+
+class RandomWordsState extends State<StatefulWidget>{
+  final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions(){
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i){
+        if(i.isOdd) return new Divider();
+        final index = i ~/ 2;
+        if(index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair){
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
+      onLongPress: () {
+        setState(() {
+          if(alreadySaved){
+            _saved.add(pair);
+          }else{
+            _saved.remove(pair);
+          }
+        });
+      },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+   final wordPair = WordPair.random();
+    return new Scaffold(
+      appBar:new AppBar(
+        title: new Text("listView title"),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+}
+
